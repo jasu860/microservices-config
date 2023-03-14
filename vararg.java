75646dc07ac9a222cf0e79bea47ecf0b5a661756
Jasleen Kaur <soniakaur.012345@gmail.com>
2023-03-14 12:14:58 +0530

    config

---
+++ vararg.java
@@ -0,0 +1,123 @@
+package com.assement2.employee;
+public class vararg {
+
+	
+//	public void m1(String s) {
+//		System.out.println("String version");
+//	}
+//	public void m1(StringBuffer sb) {
+//		System.out.println("StringBuffer version");
+//	}
+//	public void m1(int i,float f) {
+//		System.out.println("int-float version");
+//	}
+//	public void m1(float f,int i) {
+//		System.out.println("float-int version");
+//	}
+//	public void m1(Object o) {
+//		System.out.println("Object version");
+//	}
+//	public void m1(int x) {
+//		System.out.println("General method");
+//	}
+//	public void m1(int... x) {
+//		System.out.println("var-arg method");
+//	}	
+//	public static void main(String args[]) {
+//		vararg rg=new vararg();
+////		rg.m1(new Object());
+//		rg.m1();
+//		rg.m1(10,20);		
+//		rg.m1(10);
+//	    rg.m1("durga");    
+//		rg.m1(new StringBuffer("durga"));
+//		rg.m1(10, 10.5f);
+//		rg.m1(10.5f,10);
+		//rg.m1(10,10); //C.E:m1 is ambigious
+		//rg.m1(10.5f,1.5f); //C.E:m1 method not found
+		
+		//rg.m1(null);//refrence to m1 is ambigious
+	//}
+	//int x=130;
+//byte b=(byte) x;	
+	
+	
+//public static void main(String arg[]) {
+//	int [][][] x= {{{10,20,30,40},{50,60,70},{80,90}}};
+//	for(int x1[][]:x) {
+//		for(int[] x2:x1) {
+//			for(int x3:x2) {
+//				System.out.println(x3);
+//			}
+//		}
+//		
+//	}
+//	System.out.println("\n");
+//	int [][] y= {{9,10,11},{12,13,14}};
+//	for(int[]y1:y) {
+//		for(int y2:y1) {
+//			
+//			System.out.println(y2);
+//		}
+//	}
+//	int [] x= {10,20,30,40,50};
+	
+//	for(int x1:x) {
+//		System.out.println(x1);
+//	}
+//	int[][] x2= {{11,12,13},{14,15}};
+//	for(int x3[]:x2) {
+//		for(int x4:x3) {
+//			System.out.println(x4);
+//		}
+//		for(int i=0;i<10;i++) {
+//			if(i%2==0)
+//				continue;
+//	System.out.println(i);
+//		}
+//	System.out.println("\n");
+//	int a=0;
+//	do {
+//		a++;
+//		System.out.println(a);
+//		if(++a<5)
+//			continue;
+//		a++;
+//		System.out.println(a);
+//		
+//	}while(a++<10);
+//			
+//	}
+	
+//sum();
+//	sum(10,20);
+//	sum(10,20,30);
+//	sum(10,20,30,40);	
+//		byte b=10;
+
+//switch(b+1) {
+//		case 10:
+//		System.out.println("hello");
+//			break;
+//		case 100:
+//			System.out.println("hi");
+//			break;
+//		case 1000:
+//		System.out.println("we");
+//		break;
+//		}
+	
+//public static void sum(int...x) {
+//	 int total=0;
+//for(int x1:x) {
+//	total=total+x1;		
+//}
+//   System.out.println("sum :"+total); 
+//   System.out.println(true & false);
+//   System.out.println(true | false);
+//   System.out.println(true ^ false);
+//   System.out.println(~4);
+//   System.out.println(~5);
+//}
+
+}
